b479bdcf908448ae92e520006aa3a353d3be54f1
Алексей Шашин <alexshashin.2023@gmail.com>
2026-05-16 21:11:43 +0500

    Разработанная модель представления главного окна. шашин

---
+++ TaskManager_Shashin/ViewModels/VM_Pages.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using TaskManager_Shashin.Classes;
+
+namespace TaskManager_Shashin.ViewModels
+{
+    public class VM_Pages : Notification
+    {
+        public VM_Tasks vm_tasks = new VM_Tasks();
+        public VM_Pages()
+        {
+            MainWindow.init.frame.Navigate(new View.Main(vm_tasks));
+        }
+        public TaskManager_Shashin.Classes.RealyCommand OnClose
+        {
+            get
+            {
+                return new RealyCommand(obj =>
+                {
+                    MainWindow.init.Close();
+                });
+            }
+        }
+    }
+}
