11405a95c97fb7115d9ff2c4cf628a37fe38ea51
Алексей Шашин <alexshashin.2023@gmail.com>
2026-05-16 21:09:40 +0500

    Разработанный контекст данных для модели задач. Шашин

---
+++ TaskManager_Shashin/Context/TasksContext.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace TaskManager_Shashin.Context
+{
+    public class TasksContext : DbContext
+    {
+        public DbSet<Models.Tasks> Tasks { get; set; }
+        public TasksContext()
+        {
+            Database.EnsureCreated();
+            Tasks.Load();
+        }
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseMySql(Classes.Database.Config.connection, Classes.Database.Config.version);
+        }
+    }
+}
