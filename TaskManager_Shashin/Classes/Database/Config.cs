d147278b623c4e356ee2a68cefcf3c9af55663b3
Алексей Шашин <alexshashin.2023@gmail.com>
2026-05-16 20:48:16 +0500

    Созданный класс Config. шашин

---
+++ TaskManager_Shashin/Classes/Database/Config.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace TaskManager_Shashin.Classes.Database
+{
+    public class Config
+    {
+        public static readonly string connection = "server=127.0.0.1;" +
+            "uid=root;" +
+            "pwd=;" +
+            "port=3307;" +
+            "database=TaskManager;";
+
+        public static readonly MySqlServerVersion version =
+            new MySqlServerVersion(new Version(8, 0, 31));
+    }
+}
