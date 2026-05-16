4c7dab7ab7dab52519d275997ee22d76fbed3b26
Алексей Шашин <alexshashin.2023@gmail.com>
2026-05-16 21:04:52 +0500

    Разработанная модель задач. Шашин

---
+++ TaskManager_Shashin/Models/Tasks.cs
@@ -0,0 +1,171 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using TaskManager_Shashin.Classes;
+using System.Text.RegularExpressions;
+using System.Windows;
+using Schema = System.ComponentModel.DataAnnotations.Schema;
+using Microsoft.EntityFrameworkCore.Metadata;
+
+namespace TaskManager_Shashin.Models
+{
+    public class Tasks : Notification
+    {
+        public int Id { get; set; }
+        private string name;
+
+        public string Name
+        {
+            get { return name; }
+            set
+            {
+                Match match = Regex.Match(value, @"^.{1,50}$");
+                if (!match.Success)
+                    MessageBox.Show("Наименование не должно быть пустым, и не более 50 символов.",
+                        "Некорректный ввод значения.");
+                else
+                {
+                    name = value;
+                    OnPropertyChanged("Name");
+                }
+            }
+        }
+        private string priority;
+        public string Priority
+        {
+            get { return priority; }
+            set
+            {
+                Match match = Regex.Match(value, @"^.{1,30}$");
+                if (!match.Success)
+                    MessageBox.Show("Приоритет не должен быть пустым, и не более 30 символов.",
+                        "Некорректный ввод значения.");
+                else
+                {
+                    priority = value;
+                    OnPropertyChanged("Priority");
+                }
+            }
+        }
+        private DateTime dateExecute;
+        public DateTime DateExecute
+        {
+            get { return dateExecute; }
+            set
+            {
+                if (value < DateTime.Now)
+                    MessageBox.Show("Дата выполнения не может быть меньше текущей даты.",
+                        "Некорректный ввод значения.");
+                else
+                {
+                    dateExecute = value;
+                    OnPropertyChanged("DateExecute");
+                }
+            }
+        }
+        private string comment;
+        public string Comment
+        {
+            get { return comment; }
+            set
+            {
+                Match match = Regex.Match(value, @"^.{1,1000}$");
+                if (!match.Success)
+                    MessageBox.Show("Комментарий не должен быть пустым, и не более 1000 символов.",
+                        "Некорректный ввод значения.");
+                else
+                {
+                    comment = value;
+                    OnPropertyChanged("Comment");
+                }
+            }
+        }
+        public bool done;
+        public bool Done
+        {
+            get { return done; }
+            set
+            {
+                done = value;
+                OnPropertyChanged("Done");
+                OnPropertyChanged("IsDoneText");
+            }
+        }
+
+        [Schema.NotMapped]
+        private bool isEnable;
+        [Schema.NotMapped]
+        public bool IsEnable
+        {
+            get { return isEnable; }
+            set
+            {
+                isEnable = value;
+                OnPropertyChanged("IsEnable");
+                OnPropertyChanged("IsEnableText");
+            }
+        }
+        [Schema.NotMapped]
+        public string IsEnableText
+        {
+            get
+            {
+                if (IsEnable)
+                    return "Сохранить";
+                else
+                    return "Изменить";
+            }
+        }
+        [Schema.NotMapped]
+        public string IsDoneText
+        {
+            get
+            {
+                if (Done)
+                    return "Выполнено";
+                else
+                    return "Не выполнено";
+            }
+        }
+        [Schema.NotMapped]
+        public TaskManager_Shashin.Classes.RealyCommand OnEdit
+        {
+            get
+            {
+                return new TaskManager_Shashin.Classes.RealyCommand(obj =>
+                {
+                    IsEnable = !IsEnable;
+                    if (!IsEnable)
+                        (MainWindow.init.DataContext as ViewModels.VM_Pages).vm_tasks.tasksContext.SaveChanges();
+                });
+            }
+        }
+        [Schema.NotMapped]
+        public TaskManager_Shashin.Classes.RealyCommand OnDelete
+        {
+            get
+            {
+                return new TaskManager_Shashin.Classes.RealyCommand(obj =>
+                {
+                    if (MessageBox.Show("Вы уверены, что хотите удалить эту задачу?", "Подтверждение удаления", MessageBoxButton.YesNo) == MessageBoxResult.Yes)
+                    {
+                        (MainWindow.init.DataContext as ViewModels.VM_Pages).vm_tasks.Tasks.Remove(this);
+                        (MainWindow.init.DataContext as ViewModels.VM_Pages).vm_tasks.tasksContext.Remove(this);
+                        (MainWindow.init.DataContext as ViewModels.VM_Pages).vm_tasks.tasksContext.SaveChanges();
+                    }
+                });
+            }
+        }
+        [Schema.NotMapped]
+        public TaskManager_Shashin.Classes.RealyCommand OnDone
+        {
+            get
+            {
+                return new TaskManager_Shashin.Classes.RealyCommand(obj =>
+                {
+                    Done = !Done;
+                });
+            }
+        }
+    }
+}
